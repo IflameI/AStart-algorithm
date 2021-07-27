e8af8d46e7ecf44a2703876dc351063a8021ed09
iflamei <iflamei@mail.ru>
2021-07-27 19:28:54 +0300

    Первая версия алгоритма

---
+++ sketch.js
@@ -0,0 +1,133 @@
+function removeFromArray(arr, elt) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] == elt) {
+      arr.splice(i, 1);
+    }
+  }
+}
+
+function heuristic(a, b) {
+  let d = dist(a.i, a.j, b.i, b.j);
+  return d;
+}
+
+let cols = 5;
+
+let rows = 5;
+
+let grid = new Array(cols);
+
+let openSet = [];
+let closedSet = [];
+let start;
+let end;
+let w, h;
+
+function Spot(i, j) {
+  this.i = i;
+  this.j = j;
+  this.f = 0;
+  this.g = 0;
+  this.h = 0;
+  this.neighbors = [];
+  this.show = function (col) {
+    fill(col);
+    noStroke();
+    rect(this.i * w, this.j * h, w - 1, h - 1);
+  };
+  this.addNeighbors = function (grid) {
+    let i = this.i;
+    let j = this.j;
+    if (i < cols - 1) {
+      this.neighbors.push(grid[i + 1][j]);
+    }
+    if (i > 0) {
+      this.neighbors.push(grid[i - 1][j]);
+    }
+    if (j < rows - 1) {
+      this.neighbors.push(grid[i][j + 1]);
+    }
+    if (j > 0) {
+      this.neighbors.push(grid[i][j - 1]);
+    }
+  };
+}
+
+function setup() {
+  createCanvas(400, 400);
+
+  w = width / cols;
+  h = height / rows;
+
+  // making a 2d array
+  for (let i = 0; i < cols; i++) {
+    grid[i] = new Array(rows);
+  }
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      grid[i][j] = new Spot(i, j);
+    }
+  }
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      grid[i][j].addNeighbors(grid);
+    }
+  }
+  start = grid[0][0];
+  end = grid[cols - 1][rows - 1];
+
+  openSet.push(start);
+}
+
+function draw() {
+  if (openSet.length > 0) {
+    let winner = 0;
+    for (let i = 0; i < openSet.length; i++) {
+      if (openSet[i].f < openSet[winner].f) {
+        winner = i;
+      }
+    }
+    let current = openSet[winner];
+
+    if (current === end) {
+      console.log('DONE');
+    }
+
+    removeFromArray(openSet, current);
+    closedSet.push(current);
+
+    var neighbors = current.neighbors;
+    for (let i = 0; i < neighbors.length; i++) {
+      let neighbor = neighbors[i];
+
+      if (!closedSet.includes(neighbor)) {
+        let tempG = current.g + 1;
+        if (openSet.includes(neighbor)) {
+          if (tempG < neighbor.g) {
+            neighbor.g = tempG;
+          } else {
+            neighbor.g = tempG;
+            openSet.push(neighbor);
+          }
+          neighbor.h = heuristic(neighbor, end);
+          neighbor.f = neighbor.g + neighbor.h;
+        }
+      }
+    }
+  } else {
+  }
+
+  background(0);
+
+  for (let i = 0; i < cols; i++) {
+    for (let j = 0; j < rows; j++) {
+      grid[i][j].show(color(255));
+    }
+  }
+  for (let i = 0; i < closedSet.length; i++) {
+    closedSet[i].show(color(255, 0, 0));
+  }
+  for (let i = 0; i < openSet.length; i++) {
+    openSet[i].show(color(0, 255, 0));
+  }
+}
